a9e97d02080a8b85b750512f8cfe30ad29b12234
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-04-03 21:52:56 +0100

    Add compute/sequence/steps/update.sh

---
+++ compute/sequence/steps/update.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+: << 'comment'
+This script updates a state machine. 
+comment
+
+# Path
+path=file://compute/sequence/steps
+
+# The state machine.
+aws stepfunctions update-state-machine --cli-input-json $path/update.json --definition $path/machine.asl.json
