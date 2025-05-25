e0fff5a87ce613f72d1dfb64c5aa21d5691dae72
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-05-25 14:12:44 +0100

    Add compute/measures/steps/execute.sh

---
+++ compute/measures/steps/execute.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+: << 'comment'
+This script starts the execution of a state machine via its Amazon Resource Name (ARN).
+comment
+
+path=file://compute/measures/steps
+
+aws stepfunctions start-execution --cli-input-json $path/execute.json
