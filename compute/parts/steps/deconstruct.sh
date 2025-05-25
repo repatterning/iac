db4a1056b8c60ec3776fb5057d4a59796ca4e5fa
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-05-25 12:42:39 +0100

    Update compute/parts/steps/deconstruct.sh

--- compute/parts/steps/deconstruct.sh
+++ compute/parts/steps/deconstruct.sh
@@ -15,11 +15,15 @@ aws stepfunctions delete-state-machine --cli-input-json $path/steps/delete.json
 sleep $tau
 
 # De-register Task Definition/s
-aws ecs deregister-task-definition --cli-input-json $path/ecs/tasks/deregister.json
+# aws ecs deregister-task-definition --cli-input-json $path/ecs/tasks/deregister.json
+aws ecs deregister-task-definition --task-definition $1:1
+aws ecs deregister-task-definition --task-definition $1:2
 sleep $tau
 
 # Delete Task Definition/s
-aws ecs delete-task-definitions --cli-input-json $path/ecs/tasks/delete-data.json
+# aws ecs delete-task-definitions --cli-input-json $path/ecs/tasks/delete-data.json
+aws ecs delete-task-definitions --task-definition $1:1
+aws ecs delete-task-definitions --task-definition $1:2
 sleep $tau
 
 # Delete Log Group
