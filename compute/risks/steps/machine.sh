c7c1f6d3d0f066ebc66492812706e227262993e2
greyhypotheses <5801124-greyhypotheses@users.noreply.gitlab.com>
2025-08-30 22:33:28 +0100

    Add compute/risks/steps/machine.sh

---
+++ compute/risks/steps/machine.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+: << 'comment'
+This script assumes that (a) the underpinning Amazon ECS (Elastic Container Service) Cluster 
+is active, and (b) the machine's underlying task/s is/are active. 
+comment
+
+# Path
+path=file://compute/risks/steps
+
+# The state machine.
+aws stepfunctions create-state-machine --cli-input-json $path/define.json --definition $path/machine.asl.json
