25de23e7c19a4515c2da540f8f4bf924a88d3d6d
greyhypotheses <5801124-greyhypotheses@users.noreply.gitlab.com>
2025-08-30 21:34:08 +0100

    Delete compute/extra/steps/execute.sh

--- compute/extra/steps/execute.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-: << 'comment'
-This script starts the execution of a state machine via its Amazon Resource Name (ARN).
-comment
-
-path=file://compute/extra/steps
-
-aws stepfunctions start-execution --cli-input-json $path/execute.json
